38ef00e4b60154b4dcbf1c07c0926ccd97743e6d
arpple <apple0239@gmail.com>
2021-02-04 07:04:42 +0000

    rename doom dir

---
+++ doom.d/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elixir-atom-face ((t (:inherit font-lock-constant-face :foreground "#BD63C5"))))
+ '(font-lock-comment-face ((t (:foreground "yellow" :slant italic))))
+ '(font-lock-constant-face ((t (:foreground "#BD63C5"))))
+ '(font-lock-function-name-face ((t (:foreground "#A6FF2E"))))
+ '(font-lock-variable-name-face ((t (:foreground "#3BDAAF"))))
+ '(hl-fill-column-face ((t (:background "#ee7600"))))
+ '(hl-line ((t (:background "#353535"))))
+ '(treemacs-git-ignored-face ((t (:foreground "gray45"))))
+ '(treemacs-git-untracked-face ((t (:inherit (variable-pitch font-lock-doc-face) :foreground "chartreuse1")))))
