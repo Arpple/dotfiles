f3669cb4ee242499a02d5dc707cb63401f5cf585
arpple <apple0239@gmail.com>
2022-04-06 21:39:37 +0700

    shell check file

--- setup.sh
+++ setup.sh
@@ -1,27 +1,49 @@
 #!/usr/bin/env sh
 
 # fish
-rm ~/.config/fish/config.fish
-ln -s "$PWD/config.fish" ~/.config/fish/config.fish
+FISH_FILE=~/.config/fish/config.fish
+if [ -f "$FISH_FILE" ]; then
+	rm $FISH_FILE
+fi
+ln -s "$PWD/config.fish" $FISH_FILE
 
 # alacritty
-rm -r ~/.config/alacritty
-mkdir -p ~/.config/alacritty
-ln -s "$PWD/alacritty.yml" ~/.config/alacritty/alacritty.yml
+ALACRITTY_DIR=~/.config/alacritty
+if [ -d "$ALACRITTY_DIR" ]; then
+	rm -r $ALACRITTY_DIR
+fi
+mkdir -p $ALACRITTY_DIR
+ln -s "$PWD/alacritty.yml" $ALACRITTY_DIR/alacritty.yml
 
 # starship
-rm ~/.config/starship.toml
-ln -s "$PWD/starship.toml" ~/.config/starship.toml
+STARSHIP_FILE=~/.config/starship.toml
+if [ -f "$STARSHIP_FILE" ]; then
+	rm $STARSHIP_FILE
+fi
+ln -s "$PWD/starship.toml" $STARSHIP_FILE
 
 # doom
-rm -r ~/.doom.d
-ln -s "$PWD/doom.d" ~/.doom.d
+DOOM_DIR=~/.doom.d
+if [ -d "$DOOM_DIR" ]; then
+	rm -r $DOOM_DIR
+fi
+ln -s "$PWD/doom.d" $DOOM_DIR
 
 # i3
-rm ~/.config/i3/config
-ln -s "$PWD/i3.config" ~/.config/i3/config
+I3_FILE=~/.config/i3/config
+if [ -f "$I3_FILE" ]; then
+	rm $I3_FILE
+fi
+ln -s "$PWD/i3.config" $I3_FILE
 
 # polybar
-rm ~/.config/polybar/config
-rm ~/.config/polybar/config.ini
-ln -s "$PWD/polybar.ini" ~/.config/polybar/config.ini
+POLYBAR_FILE=~/.config/polybar/config
+POLYBAR_FILE2=~/.config/polybar/config.ini
+if [ -f "$POLYBAR_FILE" ]; then
+	rm $POLYBAR_FILE
+fi
+if [ -f "$POLYBAR_FILE2" ]; then
+	rm $POLYBAR_FILE2
+fi
+
+ln -s "$PWD/polybar.ini" $POLYBAR_FILE2
