9f97ba700a11cb439af11a8fd5ca23644ae345bf
arpple <apple0239@gmail.com>
2025-07-12 03:25:59 +0900

    color

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,5 +3,12 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>bx", "<cmd>e ~/temp<cr>", { desc = "Open scratch buffer", remap = true })
+
+-- delete default buffer cycle key
 vim.keymap.del("n", "H")
 vim.keymap.del("n", "L")
+
+-- floating terminal
+vim.keymap.set("n", "<leader>T", function() Snacks.terminal() end, { desc = "Terminal (cwd)", remap = true })
+vim.keymap.set("n", "<leader>t", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)", remap = true })
+
