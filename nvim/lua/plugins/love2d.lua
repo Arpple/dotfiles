65f99c05b66bc3b0de1d01d6a785fc54b743d006
arpple <apple0239@gmail.com>
2025-07-22 10:22:11 +0900

    love2d DEV

--- nvim/lua/plugins/love2d.lua
+++ nvim/lua/plugins/love2d.lua
@@ -1,7 +1,9 @@
 return {
   "S1M0N38/love2d.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    path_to_love_bin = "DEV=1 love",
+  },
   keys = {
     { "<leader>v", ft = "lua", desc = "LÖVE" },
     { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
