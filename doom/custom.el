fd8042db2af1cdd3586554f196bbff8fbee3af35
arpple <apple0239@gmail.com>
2025-09-09 07:22:12 +0900

    up

--- doom/custom.el
+++ doom/custom.el
@@ -25,7 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(codeium/metadata/api_key "8f5c5d57-395d-4e73-b92a-a13042b9486c"))
+ '(screenshot-line-numbers-p nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
